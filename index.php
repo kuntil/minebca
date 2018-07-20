a3d9c67e848d7af4ffc1db32e8921b4ad00ddef8
awank <yophy@kendari.linux.or.id>
2018-07-20 18:32:24 +0800

    change message

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $configs =  [
 $app = new Slim\App($configs);
 
 $app->get('/', function ($request, $response) {
-	return "LINE bot SDK - blog.ashura.id";
+	return "LINE bot SDK - mineBCA active";
 });
 
 $app->post('/', function ($request, $response)
