751e523406fee9181a2e0d079d4d62144aba9974
awank <yophy@kendari.linux.or.id>
2018-07-24 12:25:56 +0800

    clean respon for json

--- index.php
+++ index.php
@@ -157,6 +157,7 @@ $app->get('/delApply/{ID}',function($request,$response,array $args){
 $app->get('/apply/',function($request,$response){
 	$pg_conn = pg_connect(pg_connection_string_from_database_url());
 	$result_ = pg_query($pg_conn, "SELECT * FROM apply_tbl ORDER BY no DESC LIMIT 5");
+	$response = array();
 	$response['errors']=0;
 	$response['apply']=array();
 	while($obj = pg_fetch_assoc($result_)){
