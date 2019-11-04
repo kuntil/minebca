92c387f3f5b1c20a41ee95b5daeab0bdba7447d8
awank <yophy@kendari.linux.or.id>
2019-11-05 02:14:20 +0800

    add new source

--- index.php
+++ index.php
@@ -13,6 +13,14 @@ $dotenv->load();
 $configs =  [
 	'settings' => ['displayErrorDetails' => true],
 ];
+
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+# Here we establish the connection. Yes, that's all.
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+
 $app = new Slim\App($configs);
 
 /* ROUTES */
